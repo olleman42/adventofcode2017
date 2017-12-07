f2410abba2a00487154c41bdec0c8b005f8c0795
Sergey Popov <spopov@rapidimages.se>
2017-12-07 09:08:41 +0100

    Day 7 part 2

--- 7/main.go
+++ 7/main.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -21,6 +22,7 @@ func main() {
 
 	// fmt.Println(string(c))
 
+	var origo xnode
 	data := strings.Split(string(c), "\n")
 	// walk through list and see if they have children,
 	for _, x := range data {
@@ -39,12 +41,51 @@ func main() {
 			// if hasChild(ynodeChildren, x)
 		}
 		if orphan {
-			fmt.Println(subject)
+			origo = subject
 		}
 	}
 
-	// if noone - you've found em
+	fmt.Println(origo)
 
+	// get the weight of children recursively - check where broken link is on the way
+	// subtower weights
+
+	// parse all programs and index them
+	prgs := map[string]*xnode{}
+	for _, x := range data {
+		xx := getComponents(x)
+		prgs[xx.name] = &xx
+	}
+
+	for _, c := range origo.children {
+		fmt.Println(c, getChildTowerWeight(*prgs[c], prgs))
+	}
+
+}
+
+func getChildTowerWeight(node xnode, prgs map[string]*xnode) int {
+	countedWeight := node.weight
+
+	if node.children != nil {
+		// throw in a check to see if unbalanced
+		childWeights := []int{}
+
+		for _, c := range node.children {
+			childWeight := getChildTowerWeight(*prgs[c], prgs)
+			childWeights = append(childWeights, childWeight)
+			// check if one child weight is broken
+			countedWeight = countedWeight + childWeight
+		}
+		for _, cw := range childWeights {
+			for _, cw2 := range childWeights {
+				if cw != cw2 {
+					fmt.Println(node, childWeights)
+				}
+			}
+		}
+	}
+
+	return countedWeight
 }
 
 func hasChild(children []string, nodeName string) bool {
@@ -58,23 +99,32 @@ func hasChild(children []string, nodeName string) bool {
 
 type xnode struct {
 	name     string
+	weight   int
 	children []string
 }
 
 func getComponents(node string) xnode {
 	comp := strings.Split(node, "->")
 
-	name := strings.TrimSpace(strings.Split(comp[0], "(")[0])
+	subcomp := strings.Split(comp[0], "(")
+	name := strings.TrimSpace(subcomp[0])
+
+	weight, err := strconv.Atoi(strings.TrimSpace(strings.Replace(subcomp[1], ")", "", -1)))
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	if len(comp) > 1 {
 		return xnode{
 			name:     name,
+			weight:   weight,
 			children: strings.Split(strings.TrimSpace(comp[1]), ", "),
 		}
 	}
 
 	return xnode{
 		name:     name,
+		weight:   weight,
 		children: nil,
 	}
 }
